687866b0be9eed69c1d099c0244f089ad38c6e4a
Colin McEwan <cmcewan93@gmail.com>
2020-11-02 14:26:10 -0500

    changed path location of layout

--- src/components/app/DefaultLayout.js
+++ src/components/app/DefaultLayout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom';
-import DefaultHeader from '../../components/shared/Header/defaultHeader'
-import Footer from '../../components/shared/Footer/footer'
+import DefaultHeader from '../shared/Header/defaultHeader'
+import Footer from '../shared/Footer/footer'
 import Contact from '../Contact/contact'
 
 //Default layout for all components other than main
