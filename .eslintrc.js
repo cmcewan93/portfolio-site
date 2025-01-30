ad5638c8fd2bc5b5582a6688e66b1bca8898fa8d
Colin McEwan <colin@button.is>
2025-01-30 16:32:28 -0500

    updated eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb',      // Airbnb config
+    'airbnb/hooks' // Airbnb React Hooks config
+  ],
+  rules: {
+    // Your custom rules here
+  },
+};
